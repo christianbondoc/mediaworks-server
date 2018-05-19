6efd44f5bfb47313c1c97ae7b834d2756b33066d
christianbondoc <christianraebondoc@hotmail.com>
2018-05-19 03:38:50 -0700

    yes

--- index.js
+++ index.js
@@ -87,7 +87,9 @@ server.listen(port, (err)=>{
 
 // ALLOW ACCESS *************
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://mediaworks-tv-app.herokuapp.com/");
+    res.header("Access-Control-Allow-Origin", "https://mediaworks-web-app.herokuapp.com/");
     res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
