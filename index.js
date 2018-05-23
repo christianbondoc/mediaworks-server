4000e8da3ea516c274820f72afc1efc95b9ebbd5
Daren Capacio <megamanv5@hotmail.com>
2018-05-22 18:49:43 -0700

    playing with index 1

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ server.listen(port, (err)=>{
 // ALLOW ACCESS *************
 app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://mediaworks-tv-app.herokuapp.com/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
